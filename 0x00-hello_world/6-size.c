b8d8914a3f09aa9e183bd2d288391eafab50b290
ruthmwinzi <ruthnzambi1@gmail.com>
2023-02-16 06:55:23 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,6 @@
 	float f;
 
 	printf("Size of a char: 1 byte(s)");
-	printf("Size of an int: 4 byte(s)");
-	printf("size of a long int: 4 byte(s)");
-	printf(Size of a long long int: 8 byte(s)");
-	printf(Size of a float: 4 byte(s)");
 return (0);
 }
 
