a064b397f98945d2bf425593152672533fd8851f
ruthmwinzi <ruthnzambi1@gmail.com>
2023-02-16 05:35:00 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types on the computer it is compiled and run on.
+ * Return: 0 (success)
+ */
+	int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: 1 byte(s)");
+return (0);
+}
+
